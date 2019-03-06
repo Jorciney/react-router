c7ee4cf7ccbf6deb1cb2df7b25382d577f717cd0
Jorciney <jorciney.dias@hotmail.com>
2019-03-06 18:09:19 +0100

    Params - and NavLink

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 
 
 // COMPONENTS
 import Home from './components/home';
 import Posts from './components/posts';
 import Profile from './components/profiles';
+import PostItem from './components/post_item';
 
 
 const App = () => {
@@ -22,9 +23,18 @@ const App = () => {
                         search: "profile=true"
                     }} >Profile</Link><br/>
                     <hr/>
+                    <NavLink to="/">Home</NavLink><br/>
+                    <NavLink to={"/posts"} activeStyle={{color:'red'}}>Posts</NavLink><br/>
+                    <NavLink to={{
+                        pathname: "/profile",
+                        hash: "#jdc",
+                        search: "profile=true"
+                    }} >Profile</NavLink><br/>
+
                 </header>
                 <Route path="/" exact component={Home}/>
                 <Route path="/posts" component={Posts}/>
+                <Route path="/posts/:id/:username" component={PostItem}/>
                 <Route path="/profile" component={Profile}/>
             </div>
         </BrowserRouter>
